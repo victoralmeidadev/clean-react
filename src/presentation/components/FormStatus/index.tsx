42c10040c59f969de92fe9c32ba390f510e0e1d8
Victor Almeida <victooralmeida1@gmail.com>
2021-08-19 21:29:44 -0300

    feat: ensure login calls validation with correct email

--- src/presentation/components/FormStatus/index.tsx
+++ src/presentation/components/FormStatus/index.tsx
@@ -4,14 +4,12 @@ import Spinner from "@/presentation/components/Spinner";
 import Context from "@/presentation/contexts/form/form-context";
 
 const FormStatus: React.FC = () => {
-  const {
-    state: { isLoading },
-    errorState: { main },
-  } = useContext(Context);
+  const { state } = useContext(Context);
+  const { isLoading, mainError } = state;
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
       {isLoading && <Spinner className={Styles.spinner} />}
-      {main && <span className={Styles.error}>{main}</span>}
+      {mainError && <span className={Styles.error}>{mainError}</span>}
     </div>
   );
 };
--- src/presentation/components/Input/index.tsx
+++ src/presentation/components/Input/index.tsx
@@ -8,11 +8,17 @@ type Props = React.DetailedHTMLProps<
 >;
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context);
-  const error = errorState[props.name];
+  const { state, setState } = useContext(Context);
+  const error = state[`${props.name}Error`];
   const enableInput = useCallback(
     (event: React.FocusEvent<HTMLInputElement>): void => {
       event.target.readOnly = false;
+    },
+    []
+  );
+  const handleChange = useCallback(
+    (event: React.FocusEvent<HTMLInputElement>): void => {
+      setState({ ...state, [event.target.name]: event.target.value });
     },
     []
   );
@@ -26,7 +32,13 @@ const Input: React.FC<Props> = (props: Props) => {
   }, []);
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} readOnly onFocus={enableInput} />
+      <input
+        {...props}
+        data-testid={props.name}
+        readOnly
+        onFocus={enableInput}
+        onChange={handleChange}
+      />
       <span
         data-testid={`${props.name}-status`}
         title={getTitle()}
